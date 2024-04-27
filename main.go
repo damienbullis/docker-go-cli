f18a5b0c52b3eb98e76afbefff6567a157897c38
Damien <80228708+damienbullis@users.noreply.github.com>
2024-04-27 10:48:10 -0500

    Refactor main.go to use go-commander/cmd package for command execution

--- main.go
+++ main.go
@@ -1,49 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"go-commander/cmd"
 
-	"github.com/spf13/viper"
+	"github.com/charmbracelet/log"
 )
 
-type Config struct {
-	Key1   string `mapstructure:"key1"`
-	Key2   int    `mapstructure:"key2"`
-	Nested struct {
-		Key3 bool `mapstructure:"key3"`
-	} `mapstructure:"nested"`
-}
-
 func main() {
-	// set defaults
-	viper.SetDefault("key1", "default1")
-	viper.SetDefault("key2", 100)
-	viper.SetDefault("nested.key3", true)
-
-	// set config file
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-
-	// read config file
-	err := viper.ReadInConfig()
-	if err != nil {
-		fmt.Printf("Error reading config file: %s\n", err)
+	if err := cmd.Execute(); err != nil {
+		// Handle any errors returned by the root command execution
+		log.Fatal(err)
+		// panic(err)
 	}
-
-	// initialize config struct
-	var config Config
-
-	// unmarshal config into struct
-	if err := viper.Unmarshal(&config); err != nil {
-		fmt.Printf("Error unmarshalling config: %s\n", err)
-	}
-
-	// print config
-	fmt.Printf("Key1: %s\n", config.Key1)
-	fmt.Printf("Key2: %d\n", config.Key2)
-	fmt.Printf("Key3: %t\n", config.Nested.Key3)
-	fmt.Printf("Key1: %s\n", config.Key1)
-	fmt.Printf("Key2: %d\n", config.Key2)
-	fmt.Printf("Key3: %t\n", config.Nested.Key3)
-
 }
